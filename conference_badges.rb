00fa326ff2d7e5001763b21a6a250e409b1f5066
David Acosta <davidacosta25081@yahoo.com>
2019-01-29 23:42:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,9 +12,9 @@ badge_maker("Matz")
 
 
 
-
+new_array = []
 def batch_badge_creator (names)
-  new_array = []
+  
   names.each do |name|
        new_array << "Hello, my name is #{name}."
        end
