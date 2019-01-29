7a9577fd545ea3d5e90a7f342d5bbd5e542c9a6f
David Acosta <davidacosta25081@yahoo.com>
2019-01-29 23:08:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+def badge_maker(name)
+puts "Hello, my name is #{name}."
+end
+
+
+
+
+guests = ["Edsger", "Ada", "Charles", "Alan", "Grace","Linus","Matz"]
+guests.each do |guest|
+  badge_maker(guest)
+end
